ef7eb63ef960bcc0ed3be818e8fd3dd619df3652
Parsa <106576103+curedbylethe@users.noreply.github.com>
2023-08-14 18:46:22 +0330

    Add simple index file to check build and run

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from 'express'
+
+
+const app = express()
+const port = 8000
+
+app.get('/', (_: Request, res: Response) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
